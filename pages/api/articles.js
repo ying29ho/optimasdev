de38973bd1ca231c87cfa5bdd26301f332b48b49
ying29 <ying29ho@gmail.com>
2022-10-19 05:48:03 +0800

    news page changes

---
+++ pages/api/articles.js
@@ -0,0 +1,14 @@
+const articles=[
+    { source: {
+        id: 1,
+        name: "Borneo Bullietin"
+    },
+    author: "Azlan Othman", 
+    title:"The art and science of digital media",
+    description: "A talk on digital marketing was delivered to Laksamana College of Business students by the founder of Optimas Marketing Management Rabbani Mubarak recently.",
+    url:"https://borneobulletin.com.bn/the-art-and-science-of-digital-media/",
+    urlToImage: "https://borneobulletin.com.bn/wp-content/uploads/2022/10/P5-A_17102022.jpg",
+    publishedAt: "2022-10-17",
+    
+}
+]
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,49 @@
-function Articles() {
+import Image from "next/image";
+import MediaQuery from "react-responsive";
+
+export default function News() {
   return (
     <div>
-      <h1>Articles coming soon!</h1>
+
+    <div>
+      <MediaQuery maxWidth={900}>
+        <div className="mainContainer d-flex justify-content-center" style={{height: "100vh"}}>
+        <Image
+          src={"/images/home/header.png"}
+          layout="fill"
+          objectFit="contain"
+          // quality={100}
+          height={567}
+          width={1440}
+          alt="home page header image"
+          priority/>
+      </div>
+      </MediaQuery>
+      <MediaQuery minWidth={901}>
+        <div className="mainContainer d-flex justify-content-center">
+        <Image
+          src={"/images/home/header.png"}
+          // layout="fill"
+          objectFit="contain"
+          // quality={100}
+          height={567}
+          width={1440}
+          // layout="responsive"
+          alt="home page header image"
+        />
+      </div>
+          
+        </MediaQuery>
+    </div>
+    <div style={{width: '100%', height:"80px", backgroundColor:"#F3A712"}}>
+
+    </div>
+    <div className="container my-3 pt-5">
+<div>
+  <h1 className="mx-4 mx-md-3">News</h1>
+</div>
+    </div>
     </div>
   );
 }
 
-export default Articles;
